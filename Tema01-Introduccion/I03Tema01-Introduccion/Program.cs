f5daabf3a64366b6c15c88da8144818f970cb32e
franvelastiqui <francovelastiqui@gmail.com>
2022-09-08 22:10:03 -0300

    tercer proyecto añadido

---
+++ Tema01-Introduccion/I03Tema01-Introduccion/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace I03Tema01_Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double numero;
+			bool flag = true;
+			bool esPrimo = true;
+			string salida;
+
+			Console.WriteLine("Ingrese un numero aquí");
+			flag = double.TryParse(Console.ReadLine(), out numero);
+
+			while (flag == false)
+			{
+				Console.WriteLine("ERROR: Reingrese un numero aquí o ingrese -salir-: ");
+
+				salida = Console.ReadLine();
+
+				if (salida == "salir")
+				{
+					Environment.Exit(0);
+				}
+
+				flag = double.TryParse(salida, out numero);
+			}
+
+			do
+			{
+				for (int i = 2; i < numero; i++)
+				{
+					if (numero % i == 0)
+					{
+						esPrimo = false;
+					}
+				}
+
+				if (esPrimo == true)
+				{
+					Console.WriteLine(numero);
+				}
+
+				esPrimo = true;
+				numero -= 1;
+			}
+			while (numero > 0);
+		}
+    }
+}
