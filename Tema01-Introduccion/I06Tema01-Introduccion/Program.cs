b443936a6b5f66a3f39f5cc3fefc162533e452ef
franvelastiqui <francovelastiqui@gmail.com>
2022-09-08 23:58:53 -0300

    ejericio 6 añadido

---
+++ Tema01-Introduccion/I06Tema01-Introduccion/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace I06Tema01_Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int añoInicial;
+            int añoFinal;
+            bool flag;
+            bool esBisiesto;
+
+            Console.WriteLine("Ingrese un año inicial: ");
+            flag = int.TryParse(Console.ReadLine(), out añoInicial);
+            while(flag == false)
+            {
+                Console.WriteLine("Error, reingrese un año inicial: ");
+                flag = int.TryParse(Console.ReadLine(), out añoInicial);
+            }
+
+            Console.WriteLine("Ingrese un año final: ");
+            flag = int.TryParse(Console.ReadLine(), out añoFinal);
+            while (flag == false)
+            {
+                Console.WriteLine("Error, reingrese un año final: ");
+                flag = int.TryParse(Console.ReadLine(), out añoFinal);
+            }
+
+            for(int i = añoInicial; i<=añoFinal; i++)
+            {
+                if(i % 4 == 0 && i % 100 != 0)
+                {
+                    esBisiesto = true;
+                }
+                else if(i % 4 == 0 && i % 400 == 0)
+                {
+                    esBisiesto = true;
+                }
+                else
+                {
+                    esBisiesto = false;
+                }
+
+                if(esBisiesto == true)
+                {
+                    Console.WriteLine($"Año {i} es bisiesto");
+                }
+            }
+        }
+    }
+}
