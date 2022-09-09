cfdb48ef69e2f873367ab06c54c1ae9353faf2eb
franvelastiqui <francovelastiqui@gmail.com>
2022-09-08 22:03:03 -0300

    nuevo ejercicio 2 añadido

---
+++ Tema01-Introduccion/I02Tema01-Introduccion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace I02Tema01_Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double numero;
+			double cuadrado;
+			double cubo;
+			bool flag;
+
+			Console.WriteLine("Ingrese un numero: ");
+			flag = double.TryParse(Console.ReadLine(), out numero);
+
+			while (numero <= 0)
+			{
+				Console.WriteLine("ERROR. ¡Reingresar número!");
+				flag = double.TryParse(Console.ReadLine(), out numero);
+			}
+
+			cuadrado = Math.Pow(numero, 2);
+			cubo = Math.Pow(numero, 3);
+			Console.WriteLine("El cuadrado es {0} y el cubo es {1}", cuadrado, cubo);
+		}
+    }
+}
