008327e0f7a1fae8710c2caa1a057dcc38afc31e
franvelastiqui <francovelastiqui@gmail.com>
2022-09-08 21:43:01 -0300

    Primer ejercicio del tema 1 - Introducción a .NET y C#

---
+++ Tema01-Introduccion/I01Tema01-Introduccion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace I01Tema01_Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+            double max = 0;
+            double min = 0;
+            double sumaDeNumeros = 0;
+            double promedio;
+            bool flag;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+
+                flag = double.TryParse(Console.ReadLine(), out numero);
+
+                while (flag == false)
+                {
+                    Console.WriteLine("Error. Ingrese un numero válido: ");
+
+                    flag = double.TryParse(Console.ReadLine(), out numero);
+                }
+
+                if (i == 0 || max < numero)
+                {
+                    max = numero;
+                }
+
+                if (i == 0 || min > numero)
+                {
+                    min = numero;
+                }
+
+                sumaDeNumeros += numero;
+            }
+
+            promedio = sumaDeNumeros / 5;
+
+            Console.WriteLine("El máximo es {0}, el minimo es {1} y el promedio es {2}", max, min, promedio);
+        }
+    }
+}
