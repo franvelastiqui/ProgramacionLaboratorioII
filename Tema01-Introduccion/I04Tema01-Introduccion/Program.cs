916909ee5b896f61a03d2979115764bcb91929aa
franvelastiqui <francovelastiqui@gmail.com>
2022-09-08 22:35:55 -0300

    cuarto ejercicio

---
+++ Tema01-Introduccion/I04Tema01-Introduccion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace I04Tema01_Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+            int acumulador = 0;
+            int contador = 0;
+
+            do
+            {
+                for (int i = 1; i < numero; i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        acumulador += i;
+                    }
+                }
+
+                if (numero == acumulador)
+                {
+                    Console.WriteLine(numero);
+                    contador++;
+                }
+
+                numero += 1;
+                acumulador = 0;
+            } while (contador < 4);
+
+            
+        }
+    }
+}
