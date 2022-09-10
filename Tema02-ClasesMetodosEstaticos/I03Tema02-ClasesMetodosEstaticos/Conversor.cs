3c59a2fcf70090cc374b838840158de083c95dad
franvelastiqui <francovelastiqui@gmail.com>
2022-09-10 15:18:56 -0300

    tercer proyecto

---
+++ Tema02-ClasesMetodosEstaticos/I03Tema02-ClasesMetodosEstaticos/Conversor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I03Tema02_ClasesMetodosEstaticos
+{
+    class Conversor
+    {
+        public static string ConvertirDecimalABinario(int numeroEntero)
+        {
+            string acumulador = "";
+
+            while(numeroEntero>=1)
+            {
+                if (numeroEntero % 2 == 0)
+                {
+                    acumulador = "0" + acumulador;
+                }
+                else
+                {
+                    acumulador = "1" + acumulador;
+                }
+
+                numeroEntero /= 2;
+            }
+
+            return acumulador;
+        }
+
+        public static int ConvertirBinarioADecimal(int numeroEntero)
+        {
+            string binario;
+            int potencia;
+            int acumulador = 0;
+
+            binario = numeroEntero.ToString();
+            potencia = binario.Length - 1;
+
+            foreach(char bit in binario)
+            {
+                if(bit == '1')
+                {
+                    acumulador += 1 * (int)Math.Pow(2, potencia);
+                }
+                else
+                {
+                    acumulador += 0 * (int)Math.Pow(2, potencia);
+                }
+                potencia -= 1;
+            }
+
+            return acumulador;
+        }
+    }
+}
---
+++ Tema02-ClasesMetodosEstaticos/I03Tema02-ClasesMetodosEstaticos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace I03Tema02_ClasesMetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta;
+            int numero;
+
+            Console.WriteLine("Binario o decimal: ");
+            respuesta = Console.ReadLine();
+
+            Console.WriteLine("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(respuesta == "b")
+            {
+                Console.WriteLine(Conversor.ConvertirBinarioADecimal(numero));
+            }
+            else
+            {
+                Console.WriteLine(Conversor.ConvertirDecimalABinario(numero));
+            }
+
+        }
+    }
+}
