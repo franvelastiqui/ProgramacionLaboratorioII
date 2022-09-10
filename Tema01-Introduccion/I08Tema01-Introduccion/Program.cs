c790ea9c1d4273b6bc5e6eb16cde976842853f6b
franvelastiqui <francovelastiqui@gmail.com>
2022-09-09 21:20:05 -0300

    octavo ejercicio

---
+++ Tema01-Introduccion/I08Tema01-Introduccion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace I08Tema01_Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura;
+            string acumuladorBase = "*";
+
+            Console.WriteLine("Ingrese altura del triángulo rectángulo: ");
+            _ = int.TryParse(Console.ReadLine(), out altura);
+
+            while(altura<=0)
+            {
+                Console.WriteLine("Error. Reingrese altura del triángulo rectángulo: ");
+                _ = int.TryParse(Console.ReadLine(), out altura);
+            }
+
+            for(int i = 0; i<altura; i++)
+            {
+                Console.WriteLine(acumuladorBase);
+                acumuladorBase += "**";
+            }
+            
+        }
+    }
+}
