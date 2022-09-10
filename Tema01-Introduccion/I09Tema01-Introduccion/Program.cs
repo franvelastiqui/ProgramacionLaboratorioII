f2eab2e7172e363af98551538992cb4c070565c7
franvelastiqui <francovelastiqui@gmail.com>
2022-09-09 21:39:14 -0300

    ejercicio 9 hecho

---
+++ Tema01-Introduccion/I09Tema01-Introduccion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace I09Tema01_Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura;
+            string acumulador = "*";
+
+            Console.WriteLine("Ingrese altura del triángulo equilátero: ");
+            _ = int.TryParse(Console.ReadLine(), out altura);
+
+            while(altura<=0)
+            {
+                Console.WriteLine("Error. Reingrese altura del triángulo equilátero: ");
+                _ = int.TryParse(Console.ReadLine(), out altura);
+            }
+
+            for(int i=altura; i>0; i--)
+            {
+                Console.Write("    ");
+
+                for(int j = i-1; j>0; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(acumulador);
+
+                acumulador += "**";
+            }
+
+        }
+    }
+}
