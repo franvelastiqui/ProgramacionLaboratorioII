adbc998356f0198d14e7dc71dbe61408039f3b54
franvelastiqui <francovelastiqui@gmail.com>
2022-09-10 15:36:54 -0300

    calculadora añadida

---
+++ Tema02-ClasesMetodosEstaticos/I04Tema02-ClasesMetodosEstaticos/Calculadora.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I04Tema02_ClasesMetodosEstaticos
+{
+    class Calculadora
+    {
+        public static string Calcular(double primerOperando, double segundoOperando, char operador)
+        {
+            switch(operador)
+            {
+                case '+':
+                    return (primerOperando + segundoOperando).ToString();
+                case '-':
+                    return (primerOperando - segundoOperando).ToString();
+                case '*':
+                    return (primerOperando * segundoOperando).ToString();
+                case '/':
+                    if(Validar(segundoOperando))
+                    {
+                        return (primerOperando / segundoOperando).ToString();
+                    }
+                    return "ERROR. No se puede dividir por cero";
+                default:
+                    return "Operador no válido";
+            }
+        }
+
+        private static bool Validar(double segundoOperando)
+        {
+            if(segundoOperando == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tema02-ClasesMetodosEstaticos/I04Tema02-ClasesMetodosEstaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace I04Tema02_ClasesMetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double primerNumero;
+            double segundoNumero;
+            char operador;
+
+            Console.WriteLine("Ingrese dos números y un operador: ");
+            primerNumero = double.Parse(Console.ReadLine());
+            segundoNumero = double.Parse(Console.ReadLine());
+            operador = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Resultado = {Calculadora.Calcular(primerNumero, segundoNumero, operador)}");
+        }
+    }
+}
