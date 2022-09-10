2fb19cf13189ab682b49b72b761961d922f2a81e
franvelastiqui <francovelastiqui@gmail.com>
2022-09-10 14:32:29 -0300

    segundo proyecto

---
+++ Tema02-ClasesMetodosEstaticos/I02Tema02-ClasesMetodosEstaticos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace I02Tema02_ClasesMetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int acumulador = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese un número entero: ");
+
+                while (int.TryParse(Console.ReadLine(), out numero) == false)
+                {
+                    Console.WriteLine("Error. Reingrese un número entero: ");
+                }
+
+                acumulador += numero;
+
+                Console.WriteLine("¿Desea continuar? (S/N)");
+            } while (Validador.ValidarRespuesta(Console.ReadLine()));
+
+            Console.WriteLine($"Numero final = {acumulador}");
+        }
+    }
+}
---
+++ Tema02-ClasesMetodosEstaticos/I02Tema02-ClasesMetodosEstaticos/Validador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I02Tema02_ClasesMetodosEstaticos
+{
+    class Validador
+    {
+        public static bool ValidarRespuesta(string respuesta)
+        {
+            if(respuesta == "s" || respuesta == "S")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
