6985d30c4fc6bc490769da1ebdb36aa7e55bb024
franvelastiqui <francovelastiqui@gmail.com>
2022-09-10 16:13:08 -0300

    ejercicio de tablas

---
+++ Tema02-ClasesMetodosEstaticos/I05Tema02-ClasesMetodosEstaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace I05Tema02_ClasesMetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese un número entero: ");
+
+            while(int.TryParse(Console.ReadLine(), out numero) == false)
+            {
+                Console.WriteLine("Error. Reingrese un número entero: ");
+            }
+
+            Console.WriteLine(Tabla.DevolverTabla(numero));
+        }
+    }
+}
---
+++ Tema02-ClasesMetodosEstaticos/I05Tema02-ClasesMetodosEstaticos/Tabla.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I05Tema02_ClasesMetodosEstaticos
+{
+    class Tabla
+    {
+        public static string DevolverTabla(int numero)
+        {
+            StringBuilder acumulador = new StringBuilder();
+
+            for(int i = 1; i<10; i ++)
+            {
+                acumulador.AppendLine($"{numero} x {i} = {numero * i}");
+            }
+
+            return acumulador.ToString();
+        }
+    }
+}
