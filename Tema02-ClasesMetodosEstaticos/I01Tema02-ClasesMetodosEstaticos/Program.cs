68cd4c319912ccdea960b5cf1a33a02e725a8db9
franvelastiqui <francovelastiqui@gmail.com>
2022-09-10 14:13:36 -0300

    Segundo tema

---
+++ Tema02-ClasesMetodosEstaticos/I01Tema02-ClasesMetodosEstaticos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace I01Tema02_ClasesMetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int minimo = 0;
+            int maximo = 0;
+            int acumulador = 0;
+            int promedio;
+            bool flag = false;
+
+            Console.WriteLine("Ingrese 10 numeros enteros: ");
+
+            for(int i=0; i<10; i++)
+            {
+                while(int.TryParse(Console.ReadLine(), out numero) == false || Validador.Validar(numero, -100, 100) == false)
+                {
+                    Console.WriteLine("Error. Reingrese 10 numeros enteros: ");
+                }
+
+                acumulador += numero;
+
+                if (numero<minimo || flag == false)
+                {
+                    minimo = numero;
+                }
+
+                if (numero > maximo || flag == false)
+                {
+                    maximo = numero;
+                }
+
+                flag = true;
+
+            }
+
+            promedio = acumulador / 10;
+
+            Console.WriteLine($"Mínimo = {minimo}\tMáximo = {maximo}\tPromedio = {promedio}");
+        }
+    }
+}
---
+++ Tema02-ClasesMetodosEstaticos/I01Tema02-ClasesMetodosEstaticos/Validador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I01Tema02_ClasesMetodosEstaticos
+{
+    class Validador
+    {
+       public static bool Validar(int valor, int min, int max)
+        {
+            if(valor<-100 || valor>100)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
