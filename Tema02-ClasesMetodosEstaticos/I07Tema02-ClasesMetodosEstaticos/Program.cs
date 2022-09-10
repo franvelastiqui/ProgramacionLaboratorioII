2b04022ee9f8d1a47adcc9914d7ee971e708d64c
franvelastiqui <francovelastiqui@gmail.com>
2022-09-10 16:55:52 -0300

    Pitagoras

---
+++ Tema02-ClasesMetodosEstaticos/I07Tema02-ClasesMetodosEstaticos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace I07Tema02_ClasesMetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseT;
+            double altura;
+            double hipotenusa;
+
+            Console.Write("Base del triángulo: ");
+            baseT = double.Parse(Console.ReadLine());
+
+            Console.Write("\nAltura del triángulo: ");
+            altura = double.Parse(Console.ReadLine());
+
+            hipotenusa = Math.Sqrt(Math.Pow(baseT,2)+Math.Pow(altura,2));
+
+            Console.WriteLine($"El valor de la hiótenusa es {hipotenusa}");
+        }
+    }
+}
