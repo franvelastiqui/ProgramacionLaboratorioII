804db20a137db6b57cc3cbfa58771b1014e5ddfc
franvelastiqui <francovelastiqui@gmail.com>
2022-09-10 16:36:55 -0300

    calculadora  de area

---
+++ Tema02-ClasesMetodosEstaticos/I06Tema02-ClasesMetodosEstaticos/CalculadoraDeArea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I06Tema02_ClasesMetodosEstaticos
+{
+    class CalculadoraDeArea
+    {
+        public static double CalcularAreaCuadrado(double longitudLado)
+        {
+            return Math.Pow(longitudLado, 2);
+        }
+
+        public static double CalcularAreaTriangulo(double baseT, double altura)
+        {
+            return (baseT * altura) / 2;
+        }
+
+        public static double CalcularAreaCirculo(double radio)
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
+    }
+}
---
+++ Tema02-ClasesMetodosEstaticos/I06Tema02-ClasesMetodosEstaticos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace I06Tema02_ClasesMetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion;
+
+            Console.WriteLine("Elija el número correspondiente para calcular el área de:\n1.Cuadrado\n2.Triángulo\n3.Círculo");
+
+            opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    double lado;
+                    Console.WriteLine("Ingrese el lado del cuadrado: ");
+                    lado = double.Parse(Console.ReadLine());
+                    Console.WriteLine(CalculadoraDeArea.CalcularAreaCuadrado(lado));
+                    break;
+
+                case "2":
+                    double baseT;
+                    double altura;
+                    Console.WriteLine("Ingrese la base y luego la altura del triánglo: ");
+                    baseT = double.Parse(Console.ReadLine());
+                    altura = double.Parse(Console.ReadLine());
+                    Console.WriteLine(CalculadoraDeArea.CalcularAreaTriangulo(baseT, altura));
+                    break;
+
+                case "3":
+                    double radio;
+                    Console.WriteLine("Ingrese el radio del círculo: ");
+                    radio = double.Parse(Console.ReadLine());
+                    Console.WriteLine(CalculadoraDeArea.CalcularAreaCirculo(radio));
+                    break;
+                default:
+                    Console.WriteLine("Opcion no válida");
+                    break;
+            }
+        }
+    }
+}
