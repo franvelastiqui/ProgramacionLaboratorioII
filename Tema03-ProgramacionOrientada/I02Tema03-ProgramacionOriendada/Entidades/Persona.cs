6e1857894cc5d1c2efa79147d7988e88465bba63
franvelastiqui <francovelastiqui@gmail.com>
2022-09-10 23:37:41 -0300

    ejercicio edad añadido

---
+++ Tema03-ProgramacionOrientada/I02Tema03-ProgramacionOriendada/Entidades/Persona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        private string nombre;
+        private DateTime fechaDeNacimiento;
+        private int dni;
+
+        public Persona(string nombre, DateTime fechaDeNacimiento, int dni)
+        {
+            this.Nombre = nombre;
+            this.FechaDeNacimiento = fechaDeNacimiento;
+            this.Dni = dni;
+        }
+
+        public string Nombre
+        { 
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+        public DateTime FechaDeNacimiento
+        {
+            get { return this.fechaDeNacimiento; }
+            set { this.fechaDeNacimiento = value; }
+        }
+
+        public int Dni
+        {
+            get { return this.dni; }
+            set { this.dni = value; }
+        }
+
+        private int CalcularEdad()
+        {
+            int edad;
+
+            edad = DateTime.Now.Year - this.FechaDeNacimiento.Year;
+
+            if(this.FechaDeNacimiento.Month > DateTime.Now.Month)
+            {
+                edad -= 1;
+            }
+
+            return edad;
+        }
+
+        public string Mostrar()
+        {
+            return $"Nombre: {this.Nombre}\tFecha de nacimiento: {this.FechaDeNacimiento}\tEdad: {this.CalcularEdad()}\tDNI: {this.Dni}\n";
+        }
+
+        public string EsMayorDeEdad()
+        {
+            if(this.CalcularEdad() > 17)
+            {
+                return "Es mayor de edad";
+            }
+            return "Es menor";
+        }
+    }
+}
---
+++ Tema03-ProgramacionOrientada/I02Tema03-ProgramacionOriendada/I02Tema03-ProgramacionOriendada/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Entidades;
+
+namespace I02Tema03_ProgramacionOriendada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechaP1 = new DateTime(1998, 11, 6);
+            Persona p1 = new Persona("Juan", fechaP1, 41585289);
+
+            DateTime fechaP2 = new DateTime(1998, 7, 6);
+            Persona p2 = new Persona("Pedro", fechaP2, 45236987);
+
+            DateTime fechaP3 = new DateTime(2009, 7, 6);
+            Persona p3 = new Persona("Carla", fechaP3, 47896321);
+
+            Persona[] personas = new Persona[3];
+
+            personas[0] = p1;
+            personas[1] = p2;
+            personas[2] = p3;
+
+            for (int i = 0; i<3; i++)
+            {
+                if (String.Equals(personas[i].EsMayorDeEdad(), "Es mayor de edad"))
+                {
+                    Console.WriteLine(personas[i].Mostrar());
+                }
+            }
+        }
+    }
+}
