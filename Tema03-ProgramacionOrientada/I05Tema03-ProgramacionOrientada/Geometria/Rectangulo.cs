a1a9fd4880254226fe59554e090e32f25ec2635b
franvelastiqui <francovelastiqui@gmail.com>
2022-09-11 14:22:39 -0300

    nuevos ejercicios

---
+++ Tema03-ProgramacionOrientada/I05Tema03-ProgramacionOrientada/Geometria/Rectangulo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rectangulo
+    {
+        private float area;
+        private float perimetro;
+        private Punto vertice1;
+        private Punto vertice2;
+        private Punto vertice3;
+        private Punto vertice4;
+
+        public Rectangulo(Punto vertice1, Punto vertice3)
+        {
+            this.vertice1 = vertice1;
+            this.vertice2 =  new Punto (vertice3.X, vertice1.Y);
+            this.vertice3 = vertice3;
+            this.vertice4 = new Punto(vertice1.X, vertice3.Y);
+        }
+
+        public float Area
+        {
+            get
+            {
+                if(area == 0)
+                {
+                    this.area = (Math.Abs(vertice1.X) + Math.Abs(vertice2.X)) * (Math.Abs(vertice1.Y) + Math.Abs(vertice4.Y));
+                }
+                return this.area;
+            }
+        }
+
+        public float Perimetro
+        {
+            get
+            {
+                if(perimetro == 0)
+                {
+                    this.perimetro = ((Math.Abs(vertice1.X) + Math.Abs(vertice2.X)) + (Math.Abs(vertice1.Y) + Math.Abs(vertice4.Y))) / 2;
+                }
+                return perimetro;
+            }
+        }
+    }
+}
---
+++ Tema03-ProgramacionOrientada/I05Tema03-ProgramacionOrientada/I05Tema03-ProgramacionOrientada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Geometria;
+
+namespace I05Tema03_ProgramacionOrientada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto punto1 = new Punto(-1, 2);
+            Punto punto2 = new Punto(6, -1);
+
+            Rectangulo miRectangulo = new Rectangulo(punto1, punto2);
+
+            Mostrar(miRectangulo);
+        }
+
+        public static void Mostrar(Rectangulo figura)
+        {
+            Console.WriteLine($"Area: {figura.Area}\nPerimetro: {figura.Perimetro}");
+        }
+    }
+}
