6852c83e1f9933c04afdbc05703a8267ba77173f
franvelastiqui <francovelastiqui@gmail.com>
2022-09-10 23:02:27 -0300

    tercer tema

---
+++ Tema03-ProgramacionOrientada/I01Tema03-ProgramacionOrientada/Entidades/Cuenta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Cuenta
+    {
+        private string titular;
+        private double cantidad;
+
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string Titular
+        {
+            get { return this.titular; }
+        }
+
+        public double Cantidad
+        {
+            get { return this.cantidad; }
+        }
+
+        public string Mostrar()
+        {
+            return $"Razón social: {this.Titular}\nMonto actual = {this.cantidad}";
+        }
+        
+        public void Ingresar(double acreditado)
+        {
+            if(acreditado>0)
+            {
+                this.cantidad += acreditado;
+            }
+        }
+
+        public void Retirar(double debitado)
+        {
+            this.cantidad -= debitado;
+        }
+    }
+}
---
+++ Tema03-ProgramacionOrientada/I01Tema03-ProgramacionOrientada/I01Tema03-ProgramacionOrientada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Entidades;
+
+namespace I01Tema03_ProgramacionOrientada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuentaJuan = new Cuenta("Juan", 125);
+
+            Console.WriteLine(cuentaJuan.Mostrar());
+
+            cuentaJuan.Ingresar(200);
+
+            Console.WriteLine(cuentaJuan.Mostrar());
+
+            cuentaJuan.Retirar(1000);
+
+            Console.WriteLine(cuentaJuan.Mostrar());
+        }
+    }
+}
