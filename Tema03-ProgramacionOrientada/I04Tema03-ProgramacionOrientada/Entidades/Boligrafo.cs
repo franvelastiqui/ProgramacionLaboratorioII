72650f7f2c6f6a9d089944b6e09ec9329540064f
franvelastiqui <francovelastiqui@gmail.com>
2022-09-11 02:46:24 -0300

    ejercicio tintas

---
+++ Tema03-ProgramacionOrientada/I04Tema03-ProgramacionOrientada/Entidades/Boligrafo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Boligrafo
+    {
+        private const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor Color
+        {
+            get
+            {
+                return this.color;
+            }
+        }
+        public short Tinta
+        {
+            get
+            {
+                return this.tinta;
+            }
+
+        }
+
+        private void SetTinta(short tinta)
+        {
+            short resultante = (short)(this.Tinta + tinta);
+
+            if(resultante >= 0 && resultante <= cantidadTintaMaxima)
+            {
+                this.tinta = resultante;
+            }
+        }
+
+        public void Recargar()
+        {
+            short aRecargar = (short)(cantidadTintaMaxima - this.Tinta);
+
+            SetTinta(aRecargar);
+        }
+
+        public bool Pintar(short gasto, out string dibujo)
+        {
+            short aGastar;
+            StringBuilder sb = new StringBuilder();
+
+            if(this.Tinta <= 0)
+            {
+                dibujo = "";
+                return false;
+            }
+            else if(this.Tinta - gasto < 0)
+            {
+                aGastar = this.Tinta;
+                SetTinta((short)(this.Tinta * (-1)));
+            }
+            else
+            {
+                SetTinta((short)(gasto * (-1)));
+                aGastar = gasto;
+            }
+
+            for(int i = 0; i<aGastar; i++)
+            {
+                sb.Append("*");
+            }
+
+            dibujo = sb.ToString();
+            return true;
+        }
+
+    }
+}
---
+++ Tema03-ProgramacionOrientada/I04Tema03-ProgramacionOrientada/I04Tema03-ProgramacionOrientada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Entidades;
+
+namespace I04Tema03_ProgramacionOrientada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gasto;
+            int acumulador = 0;
+
+            //Boligrafo azul = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo rojo = new Boligrafo(ConsoleColor.Red, 50);
+
+            _ = rojo.Pintar(51, out gasto);
+            Console.WriteLine($"La tinta es {rojo.Tinta}");
+            Console.WriteLine(gasto);
+            Console.WriteLine(acumulador);
+            rojo.Recargar();
+            Console.WriteLine($"La tinta es {rojo.Tinta}");
+        }
+    }
+}
