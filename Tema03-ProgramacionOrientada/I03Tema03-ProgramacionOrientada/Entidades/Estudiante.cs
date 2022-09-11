9a49765e2adf864c1630a26cc80a8f6d869fc79e
franvelastiqui <francovelastiqui@gmail.com>
2022-09-11 01:21:01 -0300

    ejercicio universal

---
+++ Tema03-ProgramacionOrientada/I03Tema03-ProgramacionOrientada/Entidades/Estudiante.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        static Estudiante()
+        {
+            random = new Random();
+        }
+
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public int NotaPrimerParcial
+        {
+            set
+            {
+                this.notaPrimerParcial = value;
+            }
+        }
+
+        public int NotaSegundoParcial
+        {
+            set
+            {
+                this.notaSegundoParcial = value;
+            }
+        }
+
+        private float CalcularPromedio()
+        {
+            return (this.notaPrimerParcial + this.notaSegundoParcial) / 2;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            if(notaPrimerParcial>=4 && notaSegundoParcial>=4)
+            {
+                return random.Next(6, 11);
+            }
+            return -1;
+        }
+
+        public void Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"{nombre} {apellido}. {legajo}\nNota 1: {notaPrimerParcial}\tNota 2: {notaSegundoParcial}\nPromedio: {CalcularPromedio()}");
+
+            if(CalcularNotaFinal() == -1)
+            {
+                sb.Append("\nAlumnos desaprobado\n");
+            }
+            else
+            {
+                sb.Append($"\nNota final: {CalcularNotaFinal()}\n");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ Tema03-ProgramacionOrientada/I03Tema03-ProgramacionOrientada/I03Tema03-ProgramacionOrientada/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Entidades;
+
+namespace I03Tema03_ProgramacionOrientada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante e1 = new Estudiante("Gomez", "a10", "Juan");
+            Estudiante e2 = new Estudiante("Perez", "a11", "Luis");
+            Estudiante e3 = new Estudiante("Diaz", "a12", "Maria");
+
+            e1.NotaPrimerParcial = 2;
+            e1.NotaSegundoParcial = 6;
+
+            e2.NotaPrimerParcial = 5;
+            e2.NotaSegundoParcial = 8;
+
+            e3.NotaPrimerParcial = 6;
+            e3.NotaSegundoParcial = 9;
+
+            e1.Mostrar();
+            e2.Mostrar();
+            e3.Mostrar();
+        }
+    }
+}
